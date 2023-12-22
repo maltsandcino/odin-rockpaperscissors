ac2ef4c557f2acba017bb68168dad3f5868d9f52
Thomas Harrison <thomasjh@gmail.com>
2023-12-22 13:13:35 +0100

    iniial scripts for game first draft

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let rock = "Rock"
-let paper = "Paper"
-let scissors = "Scissors"
+let rock = "rock"
+let paper = "paper"
+let scissors = "scissors"
+let playerScore = 0
+let computerScore = 0
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random()*3) + 1;
@@ -11,3 +13,37 @@ function getComputerChoice() {
     }
     else {return scissors}
 }
+
+function match(playerSelection, computerSelection) {
+    let p1 = playerSelection.toLowerCase
+    let p2 = computerSelection.toLowerCase
+
+    if (p1 === rock && p2 === scissors || p1 === paper && p2 === rock || p1 === scissors && p2 === paper){
+        playerScore++;
+        return `You have won this round, current score ${playerScore} to ${computerScore}`         
+    }
+    else if (p1 === p2){
+        return `This round is a tie, current score ${playerScore} to ${computerScore}`
+    }
+    else {
+        computerScore++
+        return `You have lost this round, current score ${playerScore} to ${computerScore}`
+    }
+}
+
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+    while(playerScore < 5 || computerScore < 5) {
+        let playerSelection = prompt("Rock, paper, or scissors?");
+        let computerSelection = getComputerChoice();
+        match(playerSelection, computerSelection)
+    }
+    if (playerScore === 5){
+        return `Congratulations you have won ${playerScore} to ${computerScore}`
+    }
+    else {
+        return `Sorry, the computer has bested you. Final score: ${playerScore} to ${computerScore}.`
+    }
+}
+
