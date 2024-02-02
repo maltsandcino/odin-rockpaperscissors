5fd1a69c1a71c21f375fc7357c3a93a66de35b7b
Thomas Harrison <thomasjh@gmail.com>
2024-02-02 10:17:03 +0100

    Changed from console game to browser game

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let rock = "rock"
 let paper = "paper"
 let scissors = "scissors"
-let playerScore = 0
+
 let computerScore = 0
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.option').forEach(function(button) {
+        button.onclick = function() {
+            gamePlay(button.dataset.value);
+        }
+    });
+ });
+
+
+const controls = document.querySelectorAll(".option");
+
+    controls.forEach(function(button) {
+        button.addEventListener('click', gamePlay(button.dataset.value))});
+    
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random()*3) + 1;
@@ -14,45 +29,70 @@ function getComputerChoice() {
     else {return scissors}
 }
 
-function match(playerSelection, computerSelection) {
+
+function single_match(playerSelection, computerSelection) {
     let p1 = playerSelection;
     console.log(p1);
     let p2 = computerSelection;
     console.log(p2);
+    playerScore = document.getElementById("human").innerHTML
+    computerScore = document.getElementById("computer").innerHTML
 
     if (p1 === rock && p2 === scissors || p1 === paper && p2 === rock || p1 === scissors && p2 === paper){
-        playerScore++;
-        return console.log(`You have won this round, current score ${playerScore} to ${computerScore}`)    
+        document.getElementById("human").innerHTML++;
+        if (document.getElementById("human").innerHTML == 5){
+            document.getElementById('instructions').innerHTML = `Congratulations you have won ${parseInt(playerScore) + 1} to ${computerScore}`
+            // document.getElementById("human").innerHTML = 0
+            // document.getElementById("computer").innerHTML = 0
+            return}
+        else {return document.getElementById('instructions').innerHTML =`You have won this round, current score ${parseInt(playerScore) + 1} to ${computerScore}`}    
     }
     else if (p1 === p2){
-        return console.log(`This round is a tie, current score ${playerScore} to ${computerScore}`)
+        return document.getElementById('instructions').innerHTML = `This round is a tie, current score ${playerScore} to ${computerScore}`
     }
     else if (p1 !== rock && p1 !== scissors && p1 !== paper) {
-        return console.log('You must correctly spell only rock, paper, or scissors to play. These are the only options.')
+        return document.getElementById('instructions').innerHTML = 'You must correctly spell only rock, paper, or scissors to play. These are the only options.'
     }
     else {
-        computerScore++
-        return console.log(`You have lost this round, current score ${playerScore} to ${computerScore}`)
+        document.getElementById("computer").innerHTML++
+        if(document.getElementById("computer").innerHTML == 5) {
+            document.getElementById('instructions').innerHTML = `Sorry, the computer has bested you. Final score: ${playerScore} to ${parseInt(computerScore) + 1}.`
+            // document.getElementById("human").innerHTML = 0
+            // document.getElementById("computer").innerHTML = 0}
+        }
+        else{
+        return document.getElementById('instructions').innerHTML = `You have lost this round, current score ${playerScore} to ${parseInt(computerScore) + 1}`
     }
-}
+}}
 
-function game() {
-    var ps;
-    var ps2;
-    playerScore = 0;
-    computerScore = 0;
-    while(playerScore < 5 && computerScore < 5) {
-        ps = prompt("Rock, paper, or scissors?");
-        ps2 = ps.toLowerCase();
-        let playerSelection = ps2;
-        let computerSelection = getComputerChoice();
-        match(playerSelection, computerSelection)
+// function getSelection() {
+//     let choices = document.querySelectorAll('.option');
+
+//     for(let i = 0; i < choices.length; i++){
+//         choices[i].addEventListener('click', function(){
+//            let selection = choices[i].dataset.value;
+//            return selection
+//        })
+// }}
+
+
+function gamePlay(playerChoice) {
+    
+    playerScore = document.getElementById("human").innerHTML
+    computerScore = document.getElementById("computer").innerHTML
+
+    if (playerScore == 5){
+     
+        document.getElementById("human").innerHTML = 0
+        document.getElementById("computer").innerHTML = 0
     }
-    if (playerScore === 5){
-        return `Congratulations you have won ${playerScore} to ${computerScore}`
-    }
-    else {
-        return `Sorry, the computer has bested you. Final score: ${playerScore} to ${computerScore}.`
-    }
+    else if (computerScore == 5) {
+       
+        document.getElementById("human").innerHTML = 0
+        document.getElementById("computer").innerHTML = 0}
+    // }
+    // else {
+        single_match(playerChoice, getComputerChoice())
+    // }
 }
 
